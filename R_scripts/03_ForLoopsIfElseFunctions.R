acd12627c2ec3e5815892d32a1fad97783a612bf
eabowman <eabowman@email.arizona.edu>
2017-08-08 06:20:35 -0500

    finshed first draft of script; added section on functions and updated with more detailed information

--- R_scripts/03_ForLoopsIfElseFunctions.R
+++ R_scripts/03_ForLoopsIfElseFunctions.R
@@ -15,13 +15,24 @@ gapminder <- read_delim(paste0(dat.dir, '02_gapminder.txt'),
 
 #<< For Loops >>------------------------------------------------------------------------
 
-# Used to apply the same function calls to a collection of objects
+# For loops are used to apply the same function calls to a collection of objects
+
 # For instance, say I want the avg. life expectance for each continent
 for(c in unique(gapminder$continent)){
      life.min <- min(gapminder[gapminder$continent == c, 'lifeExp'])
      life.max <- max(gapminder[gapminder$continent == c, 'lifeExp'])
      print(paste0('The life expectancy in ',c,' is ',life.min,' to ',life.max))
 }
+
+# [1] "The life expectancy in Asia is 28.801 to 82.603"
+# [1] "The life expectancy in Europe is 43.585 to 81.757"
+# [1] "The life expectancy in Africa is 23.599 to 76.442"
+
+# Here we are taking each element in unique(gapminder$continent) and sequentially 
+# assigning it to the variable c (which is completely arbitrary). The variable c is
+# then used in the code to perform a function.
+
+#--Nesting for loops -------------------------
 
 # You can nest for loops as well
 for(c in unique(gapminder$continent)){
@@ -35,6 +46,16 @@ for(c in unique(gapminder$continent)){
      }
 }
 
+# [1] "The life expectancy in 1952 in Asia is 28.801 to 65.39"
+# [1] "The life expectancy in 1957 in Asia is 30.332 to 67.84"
+# ...
+# [1] "The life expectancy in 1952 in Africa is 30 to 52.724"
+# [1] "The life expectancy in 1957 in Africa is 31.57 to 58.089"
+
+# Here for each value of c (a.k.a each value of unique(gapminder$continent)), we will
+# also loop through each value of y (a.k.a unique(gapminder$year))
+
+
 ### Problems with for loops ----
 
 # For loops are very usefule for certain data types, but at times can become very slow
@@ -44,6 +65,8 @@ for(c in unique(gapminder$continent)){
 # 1. Don't use a loop when a vectorized alternative already exists
 # 2. Don't grow objects (via c, cbind, etc) during the loop 
 # 3. Allocate an object to hold the results and fill it in during the loop
+
+# An alternative to for loops is the apply family of functions
 
 #<< apply function family >>------------------------------------------------------------
 
@@ -57,14 +80,17 @@ for(c in unique(gapminder$continent)){
 
 # Tutorial: https://www.datacamp.com/community/tutorials/r-tutorial-apply-family#gs.qC44Rnc
 
-apply(mammals[c(2:3)],2, function(x) mean(x))
+apply(mammals[c(2:3)], 2, function(x) mean(x))
+# In this function, we are applying the function mean() to the 2nd and 3rd column of
+# the mammals data frame
 # The second argument, 2, refers to columns; passing a 1 would reference rows
+# This applies the function to each column giving us a column mean
 
 # pass multiple functions using {} or c()
 apply(mammals[c(2:3)],2, function(x) { log(mean(x)) * 3})
 apply(mammals[c(2:3)],2, function(x) c(min(x), max(x), mean(x), sd(x)))
 
-# apply can even loop
+# apply can even be used in conjunction with loops
 for(c in unique(gapminder$continent)){
      for(y in unique(gapminder$year)){
           c.y <- apply(gapminder[gapminder$continent == c & gapminder$year == y,
@@ -84,6 +110,8 @@ system.time(for(c in unique(gapminder$continent)){
           
      }
 })
+# user    system  elapsed 
+# 0.101   0.008   0.107 
 
 # for loop
 system.time(for(c in unique(gapminder$continent)){
@@ -96,6 +124,9 @@ system.time(for(c in unique(gapminder$continent)){
                        life.min,' to ',life.max))
      }
 })
+# user    system  elapsed 
+# 0.159   0.012   0.169 
+
 
 # sapply
 sapply(1:3, function(x) x^2)
@@ -105,3 +136,39 @@ sapply(1:3, function(x) x^2)
 # lapply
 lapply(1:3, function(x) x^2)
 # similar to sapply, but returns a list
+
+#<< Functions >> ----------------------------------------------------------------------
+# Functions are a piece of code written to carry out a specified task; they allow you
+# to incorporate sets of instructions that you want to use mutliple times or, if you
+# have a complex set of instructions, keep it together within a small program
+
+# For example, the base R function mean() gives you a simple way to get an average;
+# when you read your script you can immediately tell what the code will do.
+
+# Without that your code would look like this:
+sum(mammals$brain)/nrow(mammals)
+mean(mammals$brain)
+
+# Let's build our own function
+# We are going to make a function that will
+
+fahr_to_kelvin <- function(temp){
+     kelvin <- ((temp -32) * (5/9) + 273.15)
+     return(kelvin)
+     # we have to use return to send results outside of the function otherwise we see no
+     # output
+}
+
+# Here we have created a function with one argument (temp) and have assigned that 
+# function to or named it 'fahr_to_kelvin'. The body of the function, between the {},
+# is what the function actually does
+
+# When we call this function, the value we input is assigned to the object temp and 
+# is fed through the code within the body.
+
+fahr_to_kelvin(32)
+# 273.15
+
+fahr_to_kelvin(212)
+# 373.15
+
